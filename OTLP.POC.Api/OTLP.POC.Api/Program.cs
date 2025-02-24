3cb6bb70a8b70583a86462beae654f84bf2b47ab
Kamal Ranaweera <ranaweer@ualberta.ca>
2025-02-23 23:23:24 -0700

    Committing two files missed from the previous commit.

--- OTLP.POC.Api/OTLP.POC.Api/Program.cs
+++ OTLP.POC.Api/OTLP.POC.Api/Program.cs
@@ -1,6 +1,58 @@
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Adding Open Telemetry.
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("POC.OpenTelemetry"))
+    .WithMetrics(options =>
+    {
+        options
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();
+
+        options.AddOtlpExporter();
+    })
+    .WithTracing(options =>
+    {
+        options
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();
+
+        options.AddOtlpExporter();
+    });
+
+
+builder.Logging.AddOpenTelemetry(options => options.AddOtlpExporter());
+
+//Enable Azure Monitor
+bool enableAzureMonitor = builder.Configuration.GetValue<bool>("AzureMonitor:Enable");
+if (enableAzureMonitor)
+{
+    // Filter out unwanted logs to reduce costs
+    builder.Logging.AddFilter((category, logLevel) =>
+    {
+        if (category!.Contains("Microsoft") || category.Contains("System"))
+        {
+            return logLevel >= LogLevel.Warning; // Drop Verbose/Debug logs for framework components
+        }
+        return logLevel >= LogLevel.Information; // Keep only important logs for your app
+    });
+
+    //Set Azure Monitor as the data storage
+    builder.Services.AddOpenTelemetry().UseAzureMonitor(options =>
+    {
+        options.ConnectionString = builder.Configuration.GetValue<string>("AzureMonitor:ConnectionString");
+    });
+}
+
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
